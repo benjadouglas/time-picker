1370a935ffcf35e759fa3d65bcc973e44916bb4f
DOUGLAS REZZONICO BENJAMÍN <82091184+benjadouglas@users.noreply.github.com>
2024-11-24 16:56:47 -0300

    refactor: enhance wheel container functionality and styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,7 @@
 	const minutes = Array.from({ length: 60 }, (value, index) => index);
 
 	let wheelContainer: HTMLDivElement;
-	let item_3: HTMLElement;
-	// let item: HTMLElement;
+
 	let selectedHour = 0;
 
 	function isElementInViewport(el: HTMLElement, container: HTMLDivElement): Boolean {
@@ -30,33 +29,48 @@
 		return elRect.top + elRect.height / 2 - containerRect.top - containerRect.height / 2;
 	}
 
-	function getX(y: number): number {
-		return Math.sin(Math.acos(Math.abs(y) / 300)) * 300;
+	function getX(y: number, r: number): number {
+		r *= 1.25;
+		return Math.sin(Math.acos(Math.abs(y) / r)) * r;
 	}
 
-	function getTh(y: number): number {
-		return (Math.asin(Math.abs(y) / 300) * 180) / Math.PI;
+	function getTh(y: number, r: number): number {
+		r *= 1.25;
+		return (Math.asin(Math.abs(y) / r) * 180) / Math.PI;
 	}
 
 	onMount(() => {
-		const item: HTMLElement = document.getElementById('5');
-		function applyTranform() {
-			const children = wheelContainer.children;
-			for (let i = 0; i < children.length; i++) {
-				const child = children[i] as HTMLElement;
-				child.style.transform = `scale(${getX(offsetToCenter(child, wheelContainer)) / 300})`;
-				child.style.rotate = `x ${getTh(offsetToCenter(child, wheelContainer))}deg`;
+		const diam = wheelContainer.getBoundingClientRect().height;
+		function logger() {
+			const item = document.getElementById('9');
+			if (item) {
+				console.log('scale:', getX(offsetToCenter(item, wheelContainer), diam / 2) / (diam / 2));
+				console.log('angle:', getTh(offsetToCenter(item, wheelContainer), diam / 2));
 			}
 		}
-		// event type, listener, options
+		const children = wheelContainer.children;
+		for (let i = 0; i < 24; i++) {
+			const child = children[i] as HTMLElement;
+			child.style.transform = `scale(${getX(offsetToCenter(child, wheelContainer), diam / 2) / (diam / 2)})`;
+			child.style.rotate = `x ${getTh(offsetToCenter(child, wheelContainer), diam / 2)}deg`;
+		}
+		function applyTranform() {
+			const children = wheelContainer.children;
+			for (let i = 0; i < 24; i++) {
+				const child = children[i] as HTMLElement;
+				child.style.transform = `scale(${getX(offsetToCenter(child, wheelContainer), diam / 2) / (diam / 2)})`;
+				child.style.rotate = `x ${getTh(offsetToCenter(child, wheelContainer), diam / 2)}deg`;
+			}
+		}
 		wheelContainer.addEventListener('scroll', applyTranform);
+		wheelContainer.addEventListener('scroll', logger);
 	});
 </script>
 
 <div class="flex items-center justify-center p-32">
 	<div
 		bind:this={wheelContainer}
-		class="wheel-container h-[300px] w-[350px] snap-y space-y-8 overflow-y-scroll py-[300px] text-5xl font-bold text-black"
+		class="wheel-container h-[100px] w-[55px] space-y-3 overflow-y-scroll py-[100px] text-3xl font-bold text-black"
 	>
 		{#each hours as hour}
 			{#if hour === 5}
@@ -70,11 +84,24 @@
 	</div>
 </div>
 
+<input type="time" />
 <p>Selected hour: {selectedHour}</p>
 
 <style>
 	.wheel-container {
+		scroll-snap-type: y proximity;
 		transform-style: preserve-3d;
 		transform: perspective(300px);
+		-ms-overflow-style: none; /* Internet Explorer 10+ */
+		scrollbar-width: none; /* Firefox */
+	}
+
+	.wheel-container::-webkit-scrollbar {
+		display: none; /* Safari and Chrome */
+	}
+
+	.wheel-item {
+		transform: scale(0);
+		rotate: x 0deg;
 	}
 </style>
